85fa47fecd2d87aae8c2783bd5ecf0fb0d6b9361
汪洋 <wang.yang@hbtown.com>
2022-08-01 19:07:18 +0800

    improve overlay interaction

--- demo/app/src/main/java/com/myvideoyun/giftrenderer/AnimationActivity.java
+++ demo/app/src/main/java/com/myvideoyun/giftrenderer/AnimationActivity.java
@@ -1,9 +1,14 @@
 package com.myvideoyun.giftrenderer;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.myvideoyun.giftrenderer.animTool.MVYAnimRenderThread;
 import com.myvideoyun.giftrenderer.animTool.MVYAnimView;
@@ -16,15 +21,42 @@ import com.myvideoyun.video.R;
 import static android.opengl.GLES20.*;
 import static com.myvideoyun.giftrenderer.gpuImage.MVYGPUImageConstants.AYGPUImageContentMode.kAYGPUImageScaleAspectFill;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AnimationActivity extends AppCompatActivity {
 
     MVYAnimView animView;
     MVYAnimHandler effectHandler;
+    Button button;
+    Switch overlaySwitch;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+        
+        // 下载图片
+        File imageFile =  new File(getExternalCacheDir(), "image.png");
+        if (!imageFile.exists()) {
+            new Thread(() -> {
+                try {
+                    downloadImageFromUrl("https://avatars.githubusercontent.com/u/45362645?v=4", imageFile.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> {
+                        Toast.makeText(this, "overlay image download failure", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            }).start();
+        }
 
         // 第一个渲染视图
         animView = findViewById(R.id.animation_preview);
@@ -40,8 +72,15 @@ public class AnimationActivity extends AppCompatActivity {
 
                     effectHandler = new MVYAnimHandler(AnimationActivity.this);
                     effectHandler.setRotateMode(MVYGPUImageConstants.AYGPUImageRotationMode.kAYGPUImageFlipVertical);
-                    effectHandler.setAssetPlayFinishListener( new MVYGPUImageEffectPlayFinishListener() {
+                    effectHandler.setAssetPlayFinishListener(new MVYGPUImageEffectPlayFinishListener() {
                         public void playFinish(int ret) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    button.setText("play");
+                                }
+                            });
+
                             Log.d("AnimationActivity", "当前特效播放完成, ret: " + ret);
                         }
                     });
@@ -60,7 +99,7 @@ public class AnimationActivity extends AppCompatActivity {
 
                     inputImageFramebuffer.activateFramebuffer();
 
-                    glClearColor(0,0,0,0);
+                    glClearColor(0, 0, 0, 0);
                     glClear(GL_COLOR_BUFFER_BIT);
 
                     if (effectHandler != null) {
@@ -99,12 +138,22 @@ public class AnimationActivity extends AppCompatActivity {
         });
         animView.setContentMode(kAYGPUImageScaleAspectFill);
 
-        findViewById(R.id.animation_play_bt).setOnClickListener(v -> {
+        button = findViewById(R.id.animation_play_bt);
+        button.setOnClickListener(v -> {
             Button button = (Button) v;
             if (button.getText().equals("play")) {
                 // 设置特效
                 effectHandler.setAssetPath(getExternalCacheDir() + "/myvideoyun/gifts/yurenjie/overlay_setting1.json");
-                effectHandler.setOverlayPath(getExternalCacheDir() + "/myvideoyun/gifts/yurenjie/xin_19.png");
+                File myImageFile =  new File(getExternalCacheDir(), "image.png");
+                if (overlaySwitch.isChecked()) {
+                    if (myImageFile.exists()) {
+                        effectHandler.setOverlayPath(myImageFile.getPath());
+                    } else {
+                        Toast.makeText(this, "overlay image download failure", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    effectHandler.setOverlayPath("");
+                }
                 effectHandler.setAssetPlayCount(1);
                 button.setText("stop");
             } else if (button.getText().equals("stop")) {
@@ -114,6 +163,36 @@ public class AnimationActivity extends AppCompatActivity {
                 button.setText("play");
             }
         });
+
+        overlaySwitch = findViewById(R.id.overlay_switch);
+    }
+
+
+    public void downloadImageFromUrl(String imageUrl, String storeImageLocalPath) throws IOException {
+        InputStream in = null;
+        Bitmap bitmap = null;
+        int responseCode = -1;
+
+        URL url = new URL(imageUrl);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoInput(true);
+        con.connect();
+        responseCode = con.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            in = con.getInputStream();
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        }
+
+        if (bitmap != null) {
+            try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(storeImageLocalPath))) {
+                if (bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos)) {
+                    bos.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
